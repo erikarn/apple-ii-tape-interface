d2b085417d2acd445474e66cb1887d6b1a0464ce
Adrian Chadd <adrian.chadd@fb.com>
2021-08-16 17:36:20 -0700

    Yeah, time to go use a 16x2 i2c device instead.

--- src/apple_ii_tape_interface/display.ino
+++ src/apple_ii_tape_interface/display.ino
@@ -1,16 +1,14 @@
-//#include <OakOLED.h>
 
-//extern OakOLED oled;
+//#include <LiquidCrystal_I2C.h>
+
+//LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void
 display_setup(void)
 {
-   // oled.begin();
 }
 
 void
 display_clear(void)
 {
- // oled.clearDisplay();
- // oled.display();
 }
